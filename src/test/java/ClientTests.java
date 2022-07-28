b9c49bb89b48fdd5e7806a95cfe3635a9ae13798
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-07-29 00:00:17 +0300

    add tests

--- src/test/java/ClientTests.java
+++ src/test/java/ClientTests.java
@@ -6,35 +6,44 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 public class ClientTests {
 
     static Client client;
-    static final File file = new File("src/test/java//testFile.log");
+    static final File file = new File("src/test/java/testFile.log");
+    static final String host = "localhost";
+    static ExecutorService executorService;
 
     @BeforeAll
-    public static void initAll() throws IOException {
-        client = new Client("localhost",
-                "src/main/java/universal/settings.txt",
-                file.getName());
-        file.createNewFile();
+    public static void initAll() {
+        executorService = Executors.newFixedThreadPool(2);
     }
 
     @BeforeEach
-    public void initEach() {
+    public void initEach() throws IOException {
+        file.createNewFile();
         System.out.println("Тест для метода класса Client запущен");
     }
 
     @AfterEach
     public void finishEach() {
+        file.delete();
         System.out.println("Тест для метода класса Client завершен");
     }
 
     @AfterAll
     public static void finishAll() {
-       file.delete();
+        executorService.shutdown();
     }
 
     @ParameterizedTest
@@ -51,11 +60,37 @@ public class ClientTests {
 
     @ParameterizedTest
     @MethodSource("sourceWriteMessage")
-    void testWriteMessage(String msg, String expected) {
-        String result = client.writeMessage(new Scanner(msg));
+    void testWriteMessage(String msg, String expected) throws ExecutionException, InterruptedException {
+        try (ServerSocketChannel serverSocket = ServerSocketChannel.open()) {
+            serverSocket.bind(new InetSocketAddress(host, 8080));
+            SocketChannel socketChannel = executorService.submit(serverSocket :: accept).get();
+            client = new Client(host,
+                    "src/main/java/universal/settings.txt",
+                    file.getName());
+            final ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
+            while (socketChannel.isConnected()) {
+
+                int bytesCount = socketChannel.read(inputBuffer);
+
+                if (bytesCount == -1) break;
+
+                final var info = new String(inputBuffer.array(), 0, bytesCount,
+                        StandardCharsets.UTF_8).split(": ");
+
+                final String message = info[1];
+
+                inputBuffer.clear();
+
+                socketChannel.write(ByteBuffer.wrap((message)
+                        .getBytes(StandardCharsets.UTF_8)));
+            }
+        } catch (IOException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        String result = executorService.submit(() -> client.writeMessage(new Scanner(msg))).get();
 
         Assertions.assertEquals(result, expected);
-
     }
 
     private static Stream<Arguments> sourceWriteMessage() {
--- src/test/java/ServerTests.java
+++ src/test/java/ServerTests.java
@@ -1,12 +1,87 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import server.Server;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 public class ServerTests {
-/*
-    @Test
-    void testProcess() {
 
-        Assertions.assertEquals();
-    }*/
+    static Server server;
+    static final File file = new File("src/test/java/testFile.log");
+    static final String host = "localhost";
+    static ExecutorService executorService;
+
+    @BeforeAll
+    public static void initAll() throws IOException {
+        server = new Server(host, "src/main/java/universal/settings.txt",
+                file.getName());
+        server.start();
+        executorService = Executors.newFixedThreadPool(2);
+    }
+
+    @BeforeEach
+    public void initEach() throws IOException {
+        file.createNewFile();
+        System.out.println("Тест для метода класса Server запущен");
+    }
+
+    @AfterEach
+    public void finishEach() {
+        file.delete();
+        System.out.println("Тест для метода класса Server завершен");
+    }
+
+    @AfterAll
+    public static void finishAll() {
+        executorService.shutdown();
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceSendMessageToAllClients")
+    void testSendMessageToAllClients(String msg, List<String> expected) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            executorService.submit(new Thread(server::process));
+        }
+        //один клиент записывает сообщение
+        try (SocketChannel socketChannel = SocketChannel.open()) {
+            socketChannel.connect(new InetSocketAddress(host, 8080));
+            socketChannel.write(ByteBuffer.wrap((msg)
+                    .getBytes(StandardCharsets.UTF_8)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //другие клиенты читают сообщение
+        for (int i = 0; i < 2; i++) {
+            try (SocketChannel socketChannel = SocketChannel.open()) {
+                socketChannel.connect(new InetSocketAddress(host, 8080));
+                ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
+                int bytesCount = socketChannel.read(inputBuffer);
+                msg = new String(inputBuffer.array(), 0, bytesCount,
+                        StandardCharsets.UTF_8);
+                result.add(msg);
+                inputBuffer.clear();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Assertions.assertEquals(result, expected);
+    }
+
+    private static Stream<Arguments> sourceSendMessageToAllClients() {
+        return Stream.of(Arguments.of("Привет", List.of("Привет", "Привет")));
+    }
 }
